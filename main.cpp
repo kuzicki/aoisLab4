e5d832966918fe80338781ed4a3da84d96e42237
kuzicki <75843463+kuzicki@users.noreply.github.com>
2024-04-13 09:14:00 +0300

    First commit

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <iomanip>
+#include "Logical.h"
+#include "KMap.h"
+
+int main() {
+	std::cout << Logical::SummatorFunctions() << std::endl;
+	std::cout << Logical::CodeToPlus9() << std::endl;
+}
